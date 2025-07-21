c8774b5f7a6fe19af3cb0f80b7a00f5efc839442
gitpritam <pritammajhi10@gmail.com>
2025-07-21 12:26:48 +0530

    data comes with req api practice

--- src/index.js
+++ src/index.js
@@ -3,19 +3,54 @@ import express from "express";
 const app = express();
 const PORT = 5001;
 
+app.use(express.json());
+
 //api
 app.get("/", (req, res) => {
   //   return res.send("Hello World!");
   return res.status(200).json({ success: true, message: "Hello World!" });
 });
 
-//api
+// //api
+// app.get("/user", (req, res) => {
+//   return res.status(200).json({
+//     success: true,
+//     message: "User endpoint",
+//     result: { name: "pritam", age: 25 },
+//   });
+// });
+
+//query params  ->   ?key=value       -> ?key=value&key2=value2&key3=value3
 app.get("/user", (req, res) => {
+  const { sort, limit, page } = req.query;
+  console.log("Query Params:", sort, limit, page);
+
   return res.status(200).json({
     success: true,
-    message: "User endpoint",
+    message: `${sort} ${limit} ${page}`,
     result: { name: "pritam", age: 25 },
   });
+});
+
+//url params  ->   /user/1
+app.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  console.log("URL Params:", id);
+
+  return res.status(200).json({
+    success: true,
+    message: `User ID: ${id}`,
+    result: { name: "pritam", age: 25 },
+  });
+});
+
+//body data
+app.post("/login", (req, res) => {
+  //   const loginData = req.body;
+  const { email, password } = req.body;
+  console.log("Login Data:", email, password);
+
+  return res.status(200).json({ success: true, message: "Login successful!" });
 });
 
 app.listen(PORT, () => {
